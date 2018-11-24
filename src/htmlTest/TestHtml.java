6b9c80336c3a670cc94967ac9820fb781172c295
powuzhong <1321613335@qq.com>
2018-11-24 10:24:04 +0800

    解析html文件

--- src/htmlTest/TestHtml.java
+++ src/htmlTest/TestHtml.java
@@ -26,16 +26,16 @@ public class TestHtml {
         }
     }
 
-    public static void getHtmlByurl() throws IOException {
+    private static void getHtmlByurl() throws IOException {
         String url = " <div class=\"list-linkclear\"><ul><li><a data-param=\"?_f=index_video_1_0\" href=\"https://tv.sohu.com/v/dXMvMjc0MzgzNjczLzExMDE3OTMzMy5zaHRtbA==.html\" target=\"_blank\">聚　焦</a><em class=\"line\">|</em><a data-param=\"?_f=index_video__1_1\" href=\"https://tv.sohu.com/v/dXMvMjc0MzgzNjczLzExMDE3OTMzMy5zaHRtbA==.html\" target=\"_blank\">雌雄大盗专偷双11快递 得手后嗨到“蹦迪”</a> </li><li><a data-param=\"?_f=index_video_2_0\" href=\"https://tv.sohu.com/v/dXMvMzA3NDE3MTE5LzExMDIwOTA3OS5zaHRtbA==.html\" target=\"_blank\">关　注</a><em class=\"line\">|</em><a data-param=\"?_f=index_video__2_1\" href=\"https://tv.sohu.com/v/dXMvMzA3NDE3MTE5LzExMDIwOTA3OS5zaHRtbA==.html\" target=\"_blank\">男歌手被指穿DG 怒剪上衣赤身离开舞台</a> </li><li><a data-param=\"?_f=index_video_3_0\" href=\"https://tv.sohu.com/v/dXMvMzMxMTkwODA1LzExMDIwODMzMS5zaHRtbA==.html\" target=\"_blank\">热　点</a><em class=\"line\">|</em><a data-param=\"?_f=index_video__3_1\" href=\"https://tv.sohu.com/v/dXMvMzMxMTkwODA1LzExMDIwODMzMS5zaHRtbA==.html\" target=\"_blank\">铸剑大师还原春秋铁艺 半年卖剑1200万</a> </li></ul></div>";
         Document doc = Jsoup.parse(url);
         // 获取网站标题
         String title = doc.title();
+        System.out.println("标题是：" + title);
         // 获取
         Elements table = doc.getElementsByClass("list-linkclear");
         Elements ulList = table.get(0).getElementsByTag("ul");
         for (int i = 0; i < ulList.size(); i++) {
-            Document ulDoc = Jsoup.parse(ulList.get(i).toString());
             Elements liList = table.get(0).getElementsByTag("li");
             for (Element str : liList) {
                 System.out.println(str.text());
